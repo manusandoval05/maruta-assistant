ba89bc07c02c82484ce11df8a421e8c0490985e3
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-06-01 20:44:06 -0600

    Add send icon to input bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -257,7 +257,9 @@
 									on:keydown={onPromptKeydown}
 								></textarea>
 								<button class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'} on:click={addMessage}>
-									<i class="fa-solid fa-paper-plane"></i>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+										<path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+									</svg>
 								</button>
 							</div>
 						</section>
@@ -267,3 +269,11 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	#prompt{
+		height: 40px;
+		min-width: 40px;
+		max-height: 200px;
+	}
+</style>
